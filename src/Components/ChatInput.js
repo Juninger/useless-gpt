d95c1b8aaa93cd8234787d7cdebf8f9c636bd26a
Juninger <marcus.juninger@gmail.com>
2023-06-03 14:53:35 +0200

    Updated component to use react-bootstrap

--- src/Components/ChatInput.js
+++ src/Components/ChatInput.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 
 
 export default function ChatInput({ onSend }) {
@@ -17,25 +20,33 @@ export default function ChatInput({ onSend }) {
     // Bootstrap Icon --> https://icons.getbootstrap.com/icons/send/
     function sendButtonIcon() {
         return (
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="1.5em" fill="currentColor" className="bi bi-send" viewBox="0 0 16 16">
                 <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
             </svg>)
     }
 
     return (
         <div className="mt-auto">
-            <div className="input-group mb-3">
-                <input
+            <InputGroup className='mb-3'>
+                <FormControl
+                    size='lg'
                     type="text"
-                    className="form-control form-control-md"
                     placeholder="Send a message..."
                     aria-describedby="button-chat"
                     value={message}
                     onChange={handleInputChange}
+                    style={{ border: '1px solid grey' }}
+
                 />
-                <button onClick={handleSendClick} className="btn btn-outline-dark btn-lg" type="button" id="button-chat">
+                <Button
+                    onClick={handleSendClick}
+                    variant="dark"
+                    id="button-chat"
+                    style={{ border: '1px solid grey' }}
+
+                >
                     {sendButtonIcon()}
-                </button>
-            </div>
+                </Button>
+            </InputGroup>
         </div>)
 }
