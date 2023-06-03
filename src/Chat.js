582f33851bf6a50b77089c4ecf5b45c26fdd0880
Juninger <marcus.juninger@gmail.com>
2023-06-03 12:09:34 +0200

    Updated layout + message examples
    - Fixed some scaling issues and added overflow scrolling inside message container
    - Added two types of message examples (user and bot)
    - Some test-functionality of how to add messages

--- src/Chat.js
+++ src/Chat.js
@@ -1,45 +1,63 @@
-function Chat() {
-  return (
-    <div className="container bg-dark-subtle w-75 mt-3 pt-3 position-relative" style={{ height: "95vh" }}>
+import { useRef, useState } from "react";
 
+function Chat() {
+
+  const inputRef = useRef();
+  const [messages, setMessages] = useState([]);
+
+  function addMessage() {
+    console.log(inputRef.current.value);
+    setMessages([...messages, inputRef.current.value])
+  };
+
+  
+
+  return (
+    <div className="container bg-dark-subtle w-85 mt-3 pt-3" style={{ minHeight: "95vh", display: "flex", flexDirection: "column" }}>
+
+      {/* TODO: Add header maybe? */}
       <h1>UselessGPT</h1>
 
-      <div id="chat-container" className="container overflow-auto">
-        {/*</div><div id="chat-container" className="container overflow-auto" style={{ height: "85vh" }}>*/}
-
-        {/* TODO: Render chat messages in here 
-                  ADD ROW SYSTEM
-                  ADD REACT BOOTSTRAP */}
-
+      <div id="chat-container" className="container overflow-auto" style={{ maxHeight: "calc(95vh - 150px)" }}>
         {/* Example of message-style below */}
-        <figure class="text-end">
-          <blockquote class="blockquote">
+        <figure className="text-end">
+          <blockquote className="blockquote">
             <p>A well-known quote, contained in a blockquote element.</p>
           </blockquote>
-          <figcaption class="blockquote-footer">
+          <figcaption className="blockquote-footer">
+            Someone famous in <cite title="Source Title">Source Title</cite>
+          </figcaption>
+        </figure>
+        <hr />
+        <figure className="text-start">
+          <blockquote className="blockquote">
+            <p>A well-known quote, contained in a blockquote element.</p>
+          </blockquote>
+          <figcaption className="blockquote-footer">
             Someone famous in <cite title="Source Title">Source Title</cite>
           </figcaption>
         </figure>
 
-        <hr></hr>
-
-        <figure class="text-start">
-          <blockquote class="blockquote">
-            <p>A well-known quote, contained in a blockquote element.</p>
+        {/* Example of how to render messages below */}
+        {messages.map((message) => (
+        <figure className="text-start">
+          <blockquote className="blockquote">
+            <p>{message}</p>
           </blockquote>
-          <figcaption class="blockquote-footer">
-            Someone famous in <cite title="Source Title">Source Title</cite>
+          <figcaption className="blockquote-footer">
+            Kanye West <cite title="Source Title">(Twitter, 2023)</cite>
           </figcaption>
         </figure>
-
-
+      ))}
       </div>
 
 
 
-      <div className="input-group position-absolute bottom-0">
-        <input type="text" className="form-control form-control-md" placeholder="Send a message..."></input>
-        <button class="btn btn-outline-dark btn-lg" type="button" id="button-chat">{`->`}</button>
+      <div className="mt-auto">
+        <div className="input-group mb-3">
+          <input ref={inputRef} type="text" className="form-control form-control-md" placeholder="Send a message..." aria-describedby="button-chat"></input>
+          <button onClick={addMessage} className="btn btn-outline-dark btn-lg" type="button" id="button-chat">{`->`}</button>
+        </div>
       </div>
     </div>
   );
