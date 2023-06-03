9032abcf888f40fbbedac3fba81673dff5a75fa3
Juninger <marcus.juninger@gmail.com>
2023-06-03 12:51:35 +0200

    Exported input functionality to component
    - Also added new component for send-icon from Bootstrap icons (https://icons.getbootstrap.com/icons/send/)

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,5 @@
 import { useRef, useState } from "react";
+import ChatInput from "./Components/ChatInput";
 
 function Chat() {
 
@@ -6,61 +7,54 @@ function Chat() {
   const [messages, setMessages] = useState([]);
 
   function addMessage() {
-    console.log(inputRef.current.value);
-    setMessages([...messages, inputRef.current.value])
+    setMessages([...messages, "A new message"]);
   };
 
   
 
-  return (
-    <div className="container bg-dark-subtle w-85 mt-3 pt-3" style={{ minHeight: "95vh", display: "flex", flexDirection: "column" }}>
+    return (
+      <div className="container bg-dark-subtle w-85 mt-3 pt-3" style={{ minHeight: "95vh", display: "flex", flexDirection: "column" }}>
 
-      {/* TODO: Add header maybe? */}
-      <h1>UselessGPT</h1>
+        {/* TODO: Add header maybe? */}
+        <h1>UselessGPT</h1>
 
-      <div id="chat-container" className="container overflow-auto" style={{ maxHeight: "calc(95vh - 150px)" }}>
-        {/* Example of message-style below */}
-        <figure className="text-end">
-          <blockquote className="blockquote">
-            <p>A well-known quote, contained in a blockquote element.</p>
-          </blockquote>
-          <figcaption className="blockquote-footer">
-            Someone famous in <cite title="Source Title">Source Title</cite>
-          </figcaption>
-        </figure>
-        <hr />
-        <figure className="text-start">
-          <blockquote className="blockquote">
-            <p>A well-known quote, contained in a blockquote element.</p>
-          </blockquote>
-          <figcaption className="blockquote-footer">
-            Someone famous in <cite title="Source Title">Source Title</cite>
-          </figcaption>
-        </figure>
+        <div id="chat-container" className="container overflow-auto" style={{ maxHeight: "calc(95vh - 150px)" }}>
+          {/* Example of message-style below */}
+          <figure className="text-end">
+            <blockquote className="blockquote">
+              <p>A well-known quote, contained in a blockquote element.</p>
+            </blockquote>
+            <figcaption className="blockquote-footer">
+              Someone famous in <cite title="Source Title">Source Title</cite>
+            </figcaption>
+          </figure>
+          <hr />
+          <figure className="text-start">
+            <blockquote className="blockquote">
+              <p>A well-known quote, contained in a blockquote element.</p>
+            </blockquote>
+            <figcaption className="blockquote-footer">
+              Someone famous in <cite title="Source Title">Source Title</cite>
+            </figcaption>
+          </figure>
 
-        {/* Example of how to render messages below */}
-        {messages.map((message) => (
-        <figure className="text-start">
-          <blockquote className="blockquote">
-            <p>{message}</p>
-          </blockquote>
-          <figcaption className="blockquote-footer">
-            Kanye West <cite title="Source Title">(Twitter, 2023)</cite>
-          </figcaption>
-        </figure>
-      ))}
-      </div>
-
-
-
-      <div className="mt-auto">
-        <div className="input-group mb-3">
-          <input ref={inputRef} type="text" className="form-control form-control-md" placeholder="Send a message..." aria-describedby="button-chat"></input>
-          <button onClick={addMessage} className="btn btn-outline-dark btn-lg" type="button" id="button-chat">{`->`}</button>
+          {/* Example of how to render messages below */}
+          {messages.map((message) => (
+            <figure className="text-start">
+              <blockquote className="blockquote">
+                <p>{message}</p>
+              </blockquote>
+              <figcaption className="blockquote-footer">
+                Kanye West <cite title="Source Title">(Twitter, 2023)</cite>
+              </figcaption>
+            </figure>
+          ))}
         </div>
+        
+        <ChatInput onSend={addMessage}></ChatInput>
+
       </div>
-    </div>
-  );
-}
+    );
+  }
 
 export default Chat;
--- src/Components/ChatInput.js
+++ src/Components/ChatInput.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import SendButtonIcon from './SendButtonIcon'
 
-export default function ChatInput() {
-  return (
-    <div>ChatInput</div>
-  )
+export default function ChatInput({ onSend }) {
+    return (
+        <div className="mt-auto">
+            <div className="input-group mb-3">
+                <input type="text" className="form-control form-control-md" placeholder="Send a message..." aria-describedby="button-chat"></input>
+                <button onClick={onSend} className="btn btn-outline-dark btn-lg" type="button" id="button-chat">
+                    <SendButtonIcon/>
+                </button>
+            </div>
+        </div>)
 }
